885028e45e16aa9549aac2ff1def80ad4fe3b91b
Kim_Seong_Gyu <tjdrb200@naver.com>
2022-08-14 18:25:07 +0900

    1

--- src/main/java/com/example/dreampicturespring/repository/AdminRepository.java
+++ src/main/java/com/example/dreampicturespring/repository/AdminRepository.java
@@ -7,11 +7,11 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRepository extends JpaRepository<Admintbl,Integer> {//테이블이름,프라이머리키 데이터타입
+public interface AdminRepository extends JpaRepository<Admintbl, Integer> {// 테이블이름,프라이머리키 데이터타입
 
-    @Query(value = "select admintbl.adminpwd from admintbl where admintbl.adminid = :adminID ", nativeQuery = true)
-    String findByadminID(@Param("adminID") String adminID);
+	@Query(value = "select admintbl.adminpwd from admintbl where admintbl.adminid = :adminID ", nativeQuery = true)
+	String findByadminID(@Param("adminID") String adminID);
 
-    @Query(value = "select admintbl.no_admin from admintbl where admintbl.adminid = :adminID ", nativeQuery = true)
-    Integer findByadminID_no(@Param("adminID") String adminID);
+	@Query(value = "select admintbl.no_admin from admintbl where admintbl.adminid = :adminID ", nativeQuery = true)
+	Integer findByadminID_no(@Param("adminID") String adminID);
 }
